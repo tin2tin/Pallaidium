8cbed9bbe91e04af0585426a6226b3c112ffe6b1
tin2tin <tintwotin@gmail.com>
2023-04-19 11:53:15 +0200

    Add batch function and dropdown to select type

--- __init__.py
+++ __init__.py
@@ -27,7 +27,7 @@ def show_system_console(show):
     SW_SHOW = 5
 
     ctypes.windll.user32.ShowWindow(
-        ctypes.windll.kernel32.GetConsoleWindow(), SW_SHOW if show else SW_HIDE
+        ctypes.windll.kernel32.GetConsoleWindow(), SW_SHOW #if show else SW_HIDE
     )
 
 
@@ -182,8 +182,8 @@ def install_modules(self):
     import_module(self, "accelerate", "accelerate")
     import_module(self, "transformers", "transformers")
     import_module(self, "cv2", "opencv_python")
-    import_module(self, "scipy", "scipy") 
-    import_module(self, "xformers", "xformers") 
+    import_module(self, "scipy", "scipy")
+    import_module(self, "xformers", "xformers")
 
 
 class GeneratorAddonPreferences(AddonPreferences):
@@ -312,17 +312,24 @@ class SEQUENCER_OT_generate_movie(Operator):
     def execute(self, context):
         if not bpy.types.Scene.generate_movie_prompt:
             return {"CANCELLED"}
+
+        show_system_console(True)
+        set_system_console_topmost(True)
+
         scene = context.scene
         seq_editor = scene.sequence_editor
         if not seq_editor:
             scene.sequence_editor_create()
-
         try:
             import torch
             from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
             from diffusers.utils import export_to_video
         except ModuleNotFoundError:
             print("Dependencies needs to be installed in the add-on preferences.")
+            self.report(
+                {"INFO"},
+                "Dependencies needs to be installed in the add-on preferences.",
+            )
             return {"CANCELLED"}
 
         current_frame = scene.frame_current
@@ -336,12 +343,12 @@ class SEQUENCER_OT_generate_movie(Operator):
         movie_num_inference_steps = scene.movie_num_inference_steps
         movie_num_guidance = scene.movie_num_guidance
 
-        wm = bpy.context.window_manager
-        tot = scene.movie_num_batch
-        wm.progress_begin(0, tot)
+        #wm = bpy.context.window_manager
+        #tot = scene.movie_num_batch
+        #wm.progress_begin(0, tot)
 
         for i in range(scene.movie_num_batch):
-            wm.progress_update(i)
+            #wm.progress_update(i)
             if i > 0:
                 empty_channel = scene.sequence_editor.active_strip.channel
                 start_frame = (
@@ -373,9 +380,12 @@ class SEQUENCER_OT_generate_movie(Operator):
             pipe.enable_vae_slicing()
 
             seed = context.scene.movie_num_seed
-            seed = (seed if not context.scene.movie_use_random else random.randint(0, 2147483647))
+            seed = (
+                seed
+                if not context.scene.movie_use_random
+                else random.randint(0, 2147483647)
+            )
             context.scene.movie_num_seed = seed
-            guidance = 7.5
 
             # Use cuda if possible
             if torch.cuda.is_available():
@@ -388,12 +398,11 @@ class SEQUENCER_OT_generate_movie(Operator):
                     generator.manual_seed(seed)
                 else:
                     generator = None
-
             video_frames = pipe(
                 prompt,
                 negative_prompt=negative_prompt,
                 num_inference_steps=movie_num_inference_steps,
-                guidance_scale = movie_num_guidance,
+                guidance_scale=movie_num_guidance,
                 height=y,
                 width=x,
                 num_frames=duration,
@@ -402,13 +411,13 @@ class SEQUENCER_OT_generate_movie(Operator):
 
             # Move to folder
             src_path = export_to_video(video_frames)
-            dst_path = dirname(realpath(__file__)) + "/" os.path.basename(src_path)
+            dst_path = dirname(realpath(__file__)) + "/" + os.path.basename(src_path)
             shutil.move(src_path, dst_path)
 
             # Add strip
             if os.path.isfile(dst_path):
                 strip = scene.sequence_editor.sequences.new_movie(
-                    name=context.scene.generate_movie_prompt+" "+str(seed),
+                    name=context.scene.generate_movie_prompt + " " + str(seed),
                     frame_start=start_frame,
                     filepath=dst_path,
                     channel=empty_channel,
@@ -419,20 +428,19 @@ class SEQUENCER_OT_generate_movie(Operator):
                     scene.frame_current = (
                         scene.sequence_editor.active_strip.frame_final_start
                     )
-                # Redraw UI to display the new strip
-                bpy.ops.wm.redraw_timer(
-                    type="DRAW_WIN_SWAP", iterations=1
-                )  # Remove this if Blender crashes: https://docs.blender.org/api/current/info_gotcha.html#can-i-redraw-during-script-execution
             else:
                 print("No resulting file found.")
+
+            # Redraw UI to display the new strip. Remove this if Blender crashes: https://docs.blender.org/api/current/info_gotcha.html#can-i-redraw-during-script-execution
+            #bpy.ops.wm.redraw_timer(type="DRAW_WIN_SWAP", iterations=1)
+
         bpy.ops.renderreminder.play_notification()
-        wm.progress_end()
+        #wm.progress_end()
         scene.frame_current = current_frame
 
         # clear the VRAM
         if torch.cuda.is_available():
             torch.cuda.empty_cache()
-
         return {"FINISHED"}
 
 
@@ -440,45 +448,55 @@ class SEQEUNCER_PT_generate_movie(Panel):
     """Generate Video using AI"""
 
     bl_idname = "SEQUENCER_PT_sequencer_generate_movie_panel"
-    bl_label = "Generate Video"
+    bl_label = "Generative AI"
     bl_space_type = "SEQUENCE_EDITOR"
     bl_region_type = "UI"
-    bl_category = "Generator"
+    bl_category = "Generative AI"
 
     def draw(self, context):
         layout = self.layout
-        layout.use_property_split = True
+        layout.use_property_split = False
         layout.use_property_decorate = False
         scene = context.scene
-        col = layout.column(align=True)
-        row = col.row()
-        row.scale_y = 1.2
-        row.prop(context.scene, "generate_movie_prompt", text="", icon="ADD")
-        row = col.row()
-        row.scale_y = 1.2
-        row.prop(
-            context.scene, "generate_movie_negative_prompt", text="", icon="REMOVE"
-        )
-        col = layout.column(align=True)
-        col.prop(context.scene, "generate_movie_x", text="X")
-        col.prop(context.scene, "generate_movie_y", text="Y")
+        type = scene.generatorai_typeselect
+        col = layout.column()
+        col.prop(context.scene, "generatorai_typeselect", text="")
 
-        col = layout.column(align=True)        
-        col.prop(context.scene, "generate_movie_frames", text="Frames")
+        layout = self.layout
+        col = layout.column(align=True)
+        col.use_property_split = True
+        col.use_property_decorate = False
+        col.scale_y = 1.2
+        col.prop(context.scene, "generate_movie_prompt", text="", icon="ADD")
+        col.prop(context.scene, "generate_movie_negative_prompt", text="", icon="REMOVE")
+ 
+        layout = self.layout
+        layout.use_property_split = True
+        layout.use_property_decorate = False       
+        if type == "movie":
+            col = layout.column(align=True)
+            col.prop(context.scene, "generate_movie_x", text="X")
+            col.prop(context.scene, "generate_movie_y", text="Y")
+        col = layout.column(align=True)
+        if type == "movie":
+            col.prop(context.scene, "generate_movie_frames", text="Frames")
         col.prop(context.scene, "movie_num_inference_steps", text="Quality Steps")
         col.prop(context.scene, "movie_num_guidance", text="Word Power")
         col.prop(context.scene, "movie_num_batch", text="Batch Count")
 
-        col = layout.column(align=True)
-        row = col.row(align=True)
-        sub_row = row.row(align=True)
-        sub_row.prop(context.scene, "movie_num_seed", text="Seed")
-        row.prop(context.scene, "movie_use_random", text="", icon="QUESTION")
-        sub_row.active = not context.scene.movie_use_random
- 
+        if type == "movie":
+            col = layout.column(align=True)
+            row = col.row(align=True)
+            sub_row = row.row(align=True)
+            sub_row.prop(context.scene, "movie_num_seed", text="Seed")
+            row.prop(context.scene, "movie_use_random", text="", icon="QUESTION")
+            sub_row.active = not context.scene.movie_use_random
         row = layout.row(align=True)
         row.scale_y = 1.1
-        row.operator("sequencer.generate_movie", text="Generate")
+        if type == "movie":
+            row.operator("sequencer.generate_movie", text="Generate")
+        if type == "audio":
+            row.operator("sequencer.generate_audio", text="Generate")
 
 
 class SEQUENCER_OT_generate_audio(Operator):
@@ -490,12 +508,18 @@ class SEQUENCER_OT_generate_audio(Operator):
     bl_options = {"REGISTER", "UNDO"}
 
     def execute(self, context):
-        if not bpy.types.Scene.generate_audio_prompt:
+        if not bpy.types.Scene.generate_movie_prompt:
+            self.report({"INFO"}, "Text prompt in the GeneratorAI tab is empty!")
             return {"CANCELLED"}
         scene = context.scene
-        seq_editor = scene.sequence_editor
-        if not seq_editor:
+
+        if not scene.sequence_editor:
             scene.sequence_editor_create()
+        current_frame = scene.frame_current
+        prompt = scene.generate_movie_prompt
+        negative_prompt = scene.generate_movie_negative_prompt
+        movie_num_inference_steps = scene.movie_num_inference_steps
+        movie_num_guidance = scene.movie_num_guidance
 
         try:
             from diffusers import AudioLDMPipeline
@@ -503,16 +527,25 @@ class SEQUENCER_OT_generate_audio(Operator):
             import scipy
         except ModuleNotFoundError:
             print("Dependencies needs to be installed in the add-on preferences.")
+            self.report(
+                {"INFO"},
+                "Dependencies needs to be installed in the add-on preferences.",
+            )
             return {"CANCELLED"}
-
         repo_id = "cvssp/audioldm"
-        pipe = AudioLDMPipeline.from_pretrained(repo_id) #, torch_dtype=torch.float16z
-        pipe = pipe.to("cuda")
+        pipe = AudioLDMPipeline.from_pretrained(repo_id)  # , torch_dtype=torch.float16z
 
-        prompt = context.scene.generate_audio_prompt
+        # Use cuda if possible
+        if torch.cuda.is_available():
+            pipe = pipe.to("cuda")
+        prompt = context.scene.generate_movie_prompt
         # Options: https://huggingface.co/docs/diffusers/main/en/api/pipelines/audioldm
-        audio = pipe(prompt, num_inference_steps=30, audio_length_in_s=10.0).audios[0]
-        #print(audio.tostring())
+        audio = pipe(
+            prompt,
+            num_inference_steps=movie_num_inference_steps,
+            audio_length_in_s=10.0,
+            guidance_scale=movie_num_guidance,
+        ).audios[0]
         filename = dirname(realpath(__file__)) + "/" + clean_path(prompt + ".wav")
         scipy.io.wavfile.write(filename, 16000, audio.transpose())
 
@@ -528,34 +561,40 @@ class SEQUENCER_OT_generate_audio(Operator):
             scene.sequence_editor.active_strip = strip
         else:
             print("No resulting file found!")
+        # clear the VRAM
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+        bpy.ops.renderreminder.play_notification()
+
         return {"FINISHED"}
 
 
-class SEQEUNCER_PT_generate_audio(Panel):
-    """Generate Audio with AI"""
+#class SEQEUNCER_PT_generate_audio(Panel):
+#    """Generate Audio with AI"""
 
-    bl_idname = "SEQUENCER_PT_sequencer_generate_audio_panel"
-    bl_label = "Generate Audio"
-    bl_space_type = "SEQUENCE_EDITOR"
-    bl_region_type = "UI"
-    bl_category = "Generator"
+#    bl_idname = "SEQUENCER_PT_sequencer_generate_audio_panel"
+#    bl_label = "Generate Audio"
+#    bl_space_type = "SEQUENCE_EDITOR"
+#    bl_region_type = "UI"
+#    bl_category = "Generative AI"
 
-    def draw(self, context):
-        layout = self.layout
-        scene = context.scene
-        row = layout.row()
-        row.scale_y = 1.2
-        row.prop(context.scene, "generate_audio_prompt", text="")
-        row = layout.row()
-        row.scale_y = 1.2
-        row.operator("sequencer.generate_audio", text="Generate Audio")
+#    def draw(self, context):
+#        layout = self.layout
+#        scene = context.scene
+#        row = layout.row()
+#        row.scale_y = 1.2
+#        row.prop(context.scene, "generate_audio_prompt", text="")
+#        row = layout.row()
+#        row.scale_y = 1.2
+#        row.operator("sequencer.generate_audio", text="Generate Audio")
 
 
 class SEQUENCER_OT_strip_to_generatorAI(Operator):
     """Convert selected text strips to GeneratorAI"""
-    bl_idname = 'sequencer.text_to_generator'
-    bl_label = 'Convert Text Strips to GeneratorAI'
-    bl_options = {'INTERNAL'}
+
+    bl_idname = "sequencer.text_to_generator"
+    bl_label = "Convert Text Strips to GeneratorAI"
+    bl_options = {"INTERNAL"}
     bl_description = "Adds selected text strips as GeneratorAI strips"
 
     @classmethod
@@ -563,39 +602,48 @@ class SEQUENCER_OT_strip_to_generatorAI(Operator):
         return context.scene and context.scene.sequence_editor
 
     def execute(self, context):
-        global global_captions
-        self.report({'INFO'}, "Text to GeneratorAI: Started.")
+        preferences = context.preferences
+        addon_prefs = preferences.addons[__name__].preferences
+        play_sound = addon_prefs.playsound
+        addon_prefs.playsound = False
         scene = context.scene
         sequencer = bpy.ops.sequencer
         sequences = bpy.context.sequences
         strips = context.selected_sequences
-        prompt = context.scene.generate_movie_prompt
+        prompt = scene.generate_movie_prompt
         current_frame = scene.frame_current
+        type = scene.generatorai_typeselect
         for strip in strips:
-            if strip.type == 'TEXT':
+            if strip.type == "TEXT":
                 if strip.text:
-                    print("Processing: "+strip.text)
+                    print("Processing: " + strip.text)
                     scene.generate_movie_prompt = strip.text
                     scene.frame_current = strip.frame_final_start
-                    sequencer.generate_movie()
-
+                    if type == "movie":
+                        sequencer.generate_movie()
+                    if type == "audio":
+                        sequencer.generate_audio()
         scene.frame_current = current_frame
         context.scene.generate_movie_prompt = prompt
+        addon_prefs.playsound = play_sound
+        bpy.ops.renderreminder.play_notification()
 
-        self.report({'INFO'}, "Text to GeneratorAI: Finished.")
-        return {'FINISHED'}
+        return {"FINISHED"}
+
 
 def panel_text_to_generatorAI(self, context):
     layout = self.layout
     layout.separator()
-    layout.operator('sequencer.text_to_generator', text='Text to GeneratorAI', icon="SHADERFX")
+    layout.operator(
+        "sequencer.text_to_generator", text="Text to GeneratorAI", icon="SHADERFX"
+    )
 
 
 classes = (
     SEQUENCER_OT_generate_movie,
     SEQUENCER_OT_generate_audio,
     SEQEUNCER_PT_generate_movie,
-    SEQEUNCER_PT_generate_audio,
+    # SEQEUNCER_PT_generate_audio,
     GeneratorAddonPreferences,
     GENERATOR_OT_sound_notification,
     SEQUENCER_OT_strip_to_generatorAI,
@@ -617,7 +665,11 @@ def register():
         name="generate_audio_prompt", default=""
     )
     bpy.types.Scene.generate_movie_x = bpy.props.IntProperty(
-        name="generate_movie_x", default=512, step=64, min=192, max=1024,
+        name="generate_movie_x",
+        default=512,
+        step=64,
+        min=192,
+        max=1024,
     )
     bpy.types.Scene.generate_movie_y = bpy.props.IntProperty(
         name="generate_movie_y",
@@ -668,8 +720,19 @@ def register():
         min=1,
         max=100,
     )
-    
+
+    bpy.types.Scene.generatorai_typeselect = bpy.props.EnumProperty(
+        name="Sound",
+        items={
+            ("movie", "Video", "Generate Video"),
+            ("audio", "Audio", "Generate Audio"),
+            # ("image", "Image", "Generate Image"),
+        },
+        default="movie",
+    )
+
     bpy.types.SEQUENCER_MT_add.append(panel_text_to_generatorAI)
+
 
 def unregister():
     for cls in classes:
@@ -683,7 +746,9 @@ def unregister():
     del bpy.types.Scene.movie_num_seed
     del bpy.types.Scene.movie_use_random
     del bpy.types.Scene.movie_num_guidance
+    del bpy.types.Scene.generatorai_typeselect
     bpy.types.SEQUENCER_MT_add.remove(panel_text_to_generatorAI)
+
 
 if __name__ == "__main__":
     register()
