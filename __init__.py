be878ceb51d5079850d1857657632ff5e9f0fc99
tintwotin <tintwotin@gmail.com>
2025-06-22 12:42:36 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ bl_info = {
     "name": "Pallaidium - Generative AI",
     "author": "tintwotin",
     "version": (2, 1),
-    "blender": (3, 4, 0),
+    "blender": (4, 5, 0),
     "location": "Video Sequence Editor > Sidebar > Generative AI",
     "description": "AI Generate media in the VSE",
     "category": "Sequencer",
